fe0400e7a609dc4a460771668c741451e8583f5c
nastalytomasz <nastalytomasz@gmail.com>
2020-12-22 19:48:08 +0100

    adding RWD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TransactionsModule } from './transactions/transactions.module';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
